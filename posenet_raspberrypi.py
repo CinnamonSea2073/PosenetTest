243e83341a18d5cb89a8db308f555d0b01591879
CinnamonSea2073 <Keimonn0329@gmail.com>
2024-01-30 16:18:15 +0900

    Edited

--- posenet_raspberrypi.py
+++ posenet_raspberrypi.py
@@ -11,16 +11,10 @@ API_BACKEND = "https://strong-kentucky-con-notebooks.trycloudflare.com/analyze_i
 
 async def capture_and_send_frames(frame):
     async with aiohttp.ClientSession() as session:
-        # _, img_encoded = cv2.imencode('.jpg', frame)
-        data = {'file': frame.tobytes()}
+        _, img_encoded = cv2.imencode('.jpg', frame)
+        data = {'file': img_encoded.tobytes()}
         async with session.post(API_BACKEND, data=data) as response:
-            result = await response.json(content_type='text/plain')
-  
-        now_hand_xy = eval(result)["result"][10]["keypoint"]
-        if abs(float(now_hand_xy[1]) - float(before_hand_xy[1])) > 0.04:
-            print("slide!============================================")
-        print(abs(float(now_hand_xy[1])))
-        before_hand_xy = now_hand_xy
+            result = await response.json(content_type='application/json')
     
         height, width, _ = frame.shape
         for i in eval(result)["result"]:
@@ -28,12 +22,21 @@ async def capture_and_send_frames(frame):
             cv2.circle(frame, (x, y), 10, (0, 255, 0), -1)
         cv2.imshow('preview', frame)
 
+        return eval(result)["result"][10]
+
 async def main():
     cap = cv2.VideoCapture(0)
     before_hand_xy = ["0.0", "0.0"]
     while True:
         ret, frame = cap.read()
-        await capture_and_send_frames(frame)
+        now_hand_xy = await capture_and_send_frames(frame)
+        if float(now_hand_xy["score"]) < 0.5:
+            continue
+        if abs(float(now_hand_xy["keypoint"][1]) - float(before_hand_xy["keypoint"][1])) > 0.04:
+            print("slide!============================================")
+        else:
+            print(abs(float(now_hand_xy["keypoint"][1])), abs(float(before_hand_xy["score"])))
+        before_hand_xy = now_hand_xy["keypoint"]
     # リソースの解放
     cap.release()
     cv2.destroyAllWindows()
